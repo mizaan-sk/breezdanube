9ac4ec72a4a6fbde47471a11bb0c8d0ce93a84c9
Mizaan Shaikh <mizaannuvoraa@gmail.com>
2025-06-18 16:30:01 +0530

    Footer Updated

--- Components/Footer.js
+++ Components/Footer.js
@@ -1,16 +1,26 @@
-import React from 'react'
+import Link from "next/link";
+import React from "react";
 
 const Footer = () => {
   return (
-   <>
-   <div className='bg-[#B18613] text-center py-1 px-2 text-white text-base font-sans'>
-   <p className="text-center text-[16px] md:text-[20px] font-sans font-extralight text-white py-1 px-2">
-  © 2025 Sobha Central. All rights reserved.
-</p>
+    <>
+      <div className="bg-[#B18613] md:pb-0 pb-15 flex items-center justify-center text-center px-2 text-white text-base font-sans">
+        <p className="text-center text-[15px] font-sans pr-5 font-extralight text-white py-1 px-2">
+          © 2025 Sobha Central. All rights reserved.
+        </p>
+        <span className="border-l h-4 pr-5 border-white "></span>
+        <p className=" mid_text text-[15px] font-extralight text-white ">
+          Designed & Developed by{" "}
+          <Link
+            href="https://www.nuvoraa.com/"
+            className="mid_text hover:underline  font-medium"
+          >
+            Nuvoraa Digital
+          </Link>
+        </p>
+      </div>
+    </>
+  );
+};
 
-   </div>
-   </>
-  )
-}
-
-export default Footer
+export default Footer;
